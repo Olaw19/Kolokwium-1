4550316e75e5bb72dc1414436e93c96737029de0
Olaw19 <aw320626@student.polsl.pl>
2025-11-18 08:50:05 +0100

    zad1

---
+++ zad1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 5 == 0 && n % 7 == 0) {
+        cout << "Aaaa Grrrrr!";
+    }
+    else if (n % 5 == 0) {
+        cout << "Aaaaa";
+    }
+    else if (n % 7 == 0) {
+        cout << "Grrrrrrrr";
+    }
+
+
+
+
+
+
+
+	return 0;
+}
